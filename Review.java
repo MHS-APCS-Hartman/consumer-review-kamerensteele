9067ee3aca5ce1feff91beb8ddc0bd7938df48c4
kamerensteele <59923669+kamerensteele@users.noreply.github.com>
2020-01-26 21:26:20 -0500

    Update Review.java
    Added the starReview() method.

--- Review.java
+++ Review.java
@@ -165,4 +165,31 @@ public class Review {
       return randomNegativeAdj();
     }
   }
+  
+   public static int starRating(String fileName)
+  {
+      double totalSentiment = totalSentiment(fileName);
+      if(totalSentiment >= 15)
+      {
+         return 4;
+      }
+      else if(totalSentiment >= 10)
+      {
+            return 3;  
+      }
+      else if(totalSentiment >= 5)
+      {
+         return 2;
+      }
+      else if(totalSentiment >= 0)
+      {
+         return 1;
+      }
+      else
+      {
+         return 0;
+      }
+  }
+
+  
 }
